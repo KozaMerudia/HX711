4cdd22a1d107700ea8135af8707fb7bb3495902c
Janet Hadjiangelova <janet.hadjiangelova@gmail.com>
2018-07-12 13:52:49 -0700

    Create motodriver.ino

---
+++ motodriver.ino
@@ -0,0 +1,28 @@
+#include
+
+const int d1a = 2;
+const int d1b = 3;
+const int e1 = 4;
+const int apin = A0;
+int aval = 0;
+byte pwmValue1 = 0;
+
+void setup ()
+{
+  pinMode(d1a, OUTPUT);
+  pinMode(d1b, OUTPUT);
+  pinMode(e1, OUTPUT);
+}
+
+void loop()
+{
+  ///float st = units;  ..ot veznata..
+  int i=0, j=0;
+  while (i<4)
+  {
+    if(st>10 && st<100)
+    {
+    
+    }
+  }
+}
